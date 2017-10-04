ea1652194a0112c5e94601661cd82dd8e33cf987
thesoorae <the.soorae@gmail.com>
2017-10-03 20:49:04 -0700

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdraFeedback::Application.config.secret_token = 'bc66803aa77e1fc692e65366f1a2b2ebc31dda58d20f417d322e9db5bc7b81ac3ba3429a84e83606d6436ca4761dcf47ef5286582ffb6303485f68207f1465d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AdraFeedback::Application.config.session_store :cookie_store, key: '_adra_feedback_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AdraFeedback::Application.config.session_store :active_record_store
