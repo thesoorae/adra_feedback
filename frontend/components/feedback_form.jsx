ef766d9bed21231632001dadebf9ea4977606996
Soo-Rae Hong <the.soorae@gmail.com>
2018-01-23 20:38:54 -0800

    Seperate form into sectinos

--- frontend/components/feedback_form.jsx
+++ frontend/components/feedback_form.jsx
@@ -5,6 +5,12 @@ class FeedbackForm extends React.Component {
   constructor(props){
     super(props)
     this.optional = this.optional.bind(this)
+    this.render_suggestion_form = this.render_suggestion_form.bind(this)
+    this.render_response_form = this.render_response_form.bind(this)
+    this.render_personnel_form = this.render_personnel_form.bind(this)
+    this.render_contact_form = this.render_contact_form.bind(this)
+    this.render_position_form = this.render_position_form.bind(this)
+    this.render_button = this.render_button.bind(this)
   }
 
   optional(str){
@@ -47,47 +53,77 @@ class FeedbackForm extends React.Component {
     )
   }
   
+  render_suggestion_form(){
+    return(
+      <div className="question_col">
+        <label for="suggestion_text"> ما هو اقتداحك او شكوه؟ </label>
+        <div className="text_container">
+          <textarea id="suggestion_text" name="suggestion_text" className="suggestion_textarea" />
+        </div>
+      </div>
+    )
+  }
+
+  render_response_form(){
+    return (
+      <div className="question_col">
+        <label for="action_requested"> {this.optional("الاستجابة المطلوبة")}</label>
+        <div className="text_container">
+          <textarea id="action_requested" name="action_requested" className="action_textarea" />
+        </div>
+      </div>
+      )
+  }
+
+  render_personnel_form(){
+    return(
+      <div className="question_col">
+        <div className="radio_question"> {this.optional("لمن توجه تعليقك؟")}  </div>
+        {this.render_personnel_radios("recipient")}
+      </div>
+    )
+  }
+
+  render_contact_form(){
+    return(
+      <div className="question_row">
+        <div className="question_row">
+          <input type="text" className="input_text" name="telephone" id="telephone" />
+          <label for="telephone"> {this.optional("رقم هاتف")}</label>
+        </div>
+
+        <div className="question_row">
+          <input type="text" className="input_text" name="location" id="location" />
+          <label for="location"> {this.optional("مدينه")}</label>
+        </div>
+
+      </div>
+    )
+  }
+
+  render_position_form(){
+    return (
+      <div className="question_col">
+        <div className="radio_question"> {this.optional("ما هو منصبك؟")}  </div>
+        {this.render_personnel_radios("reporter_position")}
+      </div>
+    )
+  }
+
+  render_button(){
+    return(
+      <input type="submit" value="إرسال" className="submit_btn" />
+    )
+  }
   render(){
     return (
       <form className="feedback_form"> 
-        <div className="question_row">
-          
-          <div className="question_col">
-            <label for="action_requested"> {this.optional("الاستجابة المطلوبة")}</label>
-            <div className="text_container">  
-              <textarea id="action_requested" name="action_requested" className="action_textarea"/>
-            </div>
-          </div>
-           <div className="question_col">
-            <label for="suggestion_text"> ما هو اقتداحك او شكوه؟ </label>
-            <div className="text_container">  
-              <textarea id="suggestion_text" name="suggestion_text" className="suggestion_textarea"/>
-            </div>
-          </div>
-        </div>
-
-        <div className="question_col">  
-          <div className="radio_question"> {this.optional("لمن توجه تعليقك؟")}  </div>
-          {this.render_personnel_radios("recipient")} 
-        </div>
-
-        <div className="question_row">
-          <div className="question_row">
-            <input type="text" className="input_text" name="telephone" id="telephone"/>
-            <label for="telephone"> {this.optional("رقم هاتف")}</label>
-          </div>
-
-          <div className="question_row">
-            <input type="text" className="input_text" name="location" id="location"/>
-            <label for="location"> {this.optional("مدينه")}</label>
-          </div>
-
-        </div>
-        <div className="question_col">
-          <div className="radio_question"> {this.optional("ما هو منصبك؟")}  </div>
-          {this.render_personnel_radios("reporter_position")}          
-        </div>
-        <input type="submit" value="إرسال" className="submit_btn"/>
+        {this.render_response_form()}
+        {this.render_suggestion_form()}
+        {this.render_personnel_form()}
+        {this.render_contact_form()}
+        {this.render_position_form()}
+        {this.render_button()}
       </form>
     )
   }
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/main';
+import configureStore from './store/store';
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
+  window.getState = store.getState;
+  window.store = store;
+  
   const root = document.getElementById('root');
   ReactDOM.render(<Main />, root)
 });
